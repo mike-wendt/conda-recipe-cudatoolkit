616f571062d88f8a0e5840723f7d681a6db6151c
Mike Wendt <mike.wendt@me.com>
2020-07-13 14:29:49 -0400

    ENH Add post install EULA messages

---
+++ post-link.bat
@@ -0,0 +1,5 @@
+REM Post install EULA message
+echo "By downloading and using the CUDA Toolkit conda packages, you accept the"
+echo "terms and conditions of the CUDA End User License Agreement (EULA):"
+echo ""
+echo "https://docs.nvidia.com/cuda/eula/index.html"

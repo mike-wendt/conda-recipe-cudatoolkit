2fd42b0e2feae2fc30a884649fcb339c14b69501
Mike Wendt <mike.wendt@me.com>
2020-07-13 16:17:59 -0400

    FIX Output post install EULA msg to stderr

--- post-link.sh
+++ post-link.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 # Post install EULA message
-echo "By downloading and using the CUDA Toolkit conda packages, you accept the"
-echo "terms and conditions of the CUDA End User License Agreement (EULA):"
-echo ""
-echo "https://docs.nvidia.com/cuda/eula/index.html"
+>&2 echo "By downloading and using the CUDA Toolkit conda packages, you accept the"
+>&2 echo "terms and conditions of the CUDA End User License Agreement (EULA):"
+>&2 echo ""
+>&2 echo "https://docs.nvidia.com/cuda/eula/index.html"

a58606bd95a6a080cf9a8940919baef52ce6de6e
Mike Wendt <mike.wendt@me.com>
2020-07-16 17:29:56 -0400

    FIX Try post-link for EULA MOTD

---
+++ post-link.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# post install EULA message
+cat >> $PREFIX/.messages.txt <<EOF
+By downloading and using the CUDA Toolkit conda packages, you accept the
+terms and conditions of the CUDA End User License Agreement (EULA):
+https://docs.nvidia.com/cuda/eula/index.html
+EOF

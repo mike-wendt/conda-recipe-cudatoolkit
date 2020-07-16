8e5fcfe75b91ff935fb054d110799222a048dfcc
Mike Wendt <mike.wendt@me.com>
2020-07-16 16:51:11 -0400

    ENH Update to pre-link and output to messages

--- post-link.bat
+++ post-link.bat
@@ -1,5 +1,5 @@
-REM Post install EULA message
-echo "By downloading and using the CUDA Toolkit conda packages, you accept the"
-echo "terms and conditions of the CUDA End User License Agreement (EULA):"
-echo ""
-echo "https://docs.nvidia.com/cuda/eula/index.html"
+REM Pre install EULA message
+echo "By downloading and using the CUDA Toolkit conda packages, you accept the">>$PREFIX/.messages.txt
+echo "terms and conditions of the CUDA End User License Agreement (EULA):">>$PREFIX/.messages.txt
+echo "">>$PREFIX/.messages.txt
+echo "https://docs.nvidia.com/cuda/eula/index.html">>$PREFIX/.messages.txt

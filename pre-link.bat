73658c60927f450d2dfc29953390fbe533cb37a5
Mike Wendt <mike.wendt@me.com>
2020-07-16 17:27:08 -0400

    Delete pre-link.bat

--- pre-link.bat
+++
@@ -1,5 +0,0 @@
-REM Pre install EULA message
-echo "By downloading and using the CUDA Toolkit conda packages, you accept the">>$PREFIX/.messages.txt
-echo "terms and conditions of the CUDA End User License Agreement (EULA):">>$PREFIX/.messages.txt
-echo "">>$PREFIX/.messages.txt
-echo "https://docs.nvidia.com/cuda/eula/index.html">>$PREFIX/.messages.txt

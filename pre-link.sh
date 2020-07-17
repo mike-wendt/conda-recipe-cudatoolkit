320dc5abab5a7216a9a9444742c93bdc0c92f1e8
Mike Wendt <mike.wendt@me.com>
2020-07-17 15:48:36 -0400

    FIX Update to a single line due to the byte string output

--- pre-link.sh
+++ pre-link.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # post install EULA message
-cat >> $PREFIX/.messages.txt <<EOF
-By downloading and using the CUDA Toolkit conda packages, you accept the
-terms and conditions of the CUDA End User License Agreement (EULA):
-https://docs.nvidia.com/cuda/eula/index.html
-EOF
+echo "By downloading and using the CUDA Toolkit conda packages, you accept the terms and conditions of the CUDA End User License Agreement (EULA): https://docs.nvidia.com/cuda/eula/index.html" > $PREFIX/.messages.txt

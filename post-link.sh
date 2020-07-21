41ba6c5d590b52b61de694b9e9144fa7a2268b50
Mike Wendt <mike.wendt@me.com>
2020-07-21 01:26:40 -0400

    FIX Prevent new line character from being added

--- post-link.sh
+++ post-link.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post install EULA message
-echo "By downloading and using the CUDA Toolkit conda packages, you accept the terms and conditions of the CUDA End User License Agreement (EULA): https://docs.nvidia.com/cuda/eula/index.html" > $PREFIX/.messages.txt
+echo -e "By downloading and using the CUDA Toolkit conda packages, you accept the terms and conditions of the CUDA End User License Agreement (EULA): https://docs.nvidia.com/cuda/eula/index.html" > $PREFIX/.messages.txt

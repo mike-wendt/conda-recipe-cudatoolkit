4a304e37ca81f0befb6f69da83d989d11c7f7507
Mike Wendt <mike.wendt@me.com>
2020-07-30 11:33:52 -0400

    ENH Add post-link scripts to show EULA acceptance msg
    Unable to use pre-link scripts as they do not show the msg and instead show a warning about pre-link scripts being removed

---
+++ post-link.bat
@@ -0,0 +1,2 @@
+REM Post install EULA message
+echo "By downloading and using the CUDA Toolkit conda packages, you accept the terms and conditions of the CUDA End User License Agreement (EULA): https://docs.nvidia.com/cuda/eula/index.html">>$PREFIX/.messages.txt
